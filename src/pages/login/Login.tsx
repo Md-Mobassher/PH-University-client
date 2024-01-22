6c0a168a75a2a14f71b6c511c457395f75021726
Md Mobassher Hossain <mobassherpautex@gmail.com>
2024-01-22 08:05:07 +0600

    redux query added

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import { Button } from 'antd';
+import {useForm  } from 'react-hook-form'
+import { useLoginMutation } from '../../redux/features/auth/authApi';
+import { useAppDispatch } from '../../redux/hooks';
+
 
 const Login = () => {
+  const dispatch = useAppDispatch();
+   const {register, handleSubmit} = useForm({
+    defaultValues: {
+      userId: 'A-0001',
+      password: 'admin123',
+    },
+   })
+   const [login, { error}] = useLoginMutation()
+
+   const onSubmit = async (data) => {
+    const userInfo = {
+      id: data.userId,
+      password: data.password,
+    }
+
+    login(userInfo)
+   }
+
   return (
-    <div>Login</div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">User ID:</label>
+        <input type="text" id= "id" {...register('id')} />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="text" id='password' {...register("password")} />
+      </div>
+
+      <Button htmlType='submit'>Login</Button>
+
+    </form>
   )
 }
 
--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -1,6 +1,4 @@
-import { NavLink } from "react-router-dom";
 import AdminDashboard from "../pages/admin/AdminDashboard";
-import { TRoute, TSidebarItem } from "../types/sidebar.types";
 import CreateAdmin from "../pages/admin/CreateAdmin";
 import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
@@ -36,30 +34,30 @@ export const adminPaths = [
   ];
   
 
-export const adminSidebarItems = adminPaths.reduce(
-  (acc: TSidebarItem[], item) => {
-    if (item.path && item.name) {
-      acc.push({
-        key: item.name,
-        label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
-      });
-    }
+// export const adminSidebarItems = adminPaths.reduce(
+//   (acc: TSidebarItem[], item) => {
+//     if (item.path && item.name) {
+//       acc.push({
+//         key: item.name,
+//         label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
+//       });
+//     }
 
-    if (item.children) {
-      acc.push({
-        key: item.name,
-        label: item.name,
-        children: item.children.map((child) => ({
-          key: child.name,
-          label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
-        })),
-      });
-    }
+//     if (item.children) {
+//       acc.push({
+//         key: item.name,
+//         label: item.name,
+//         children: item.children.map((child) => ({
+//           key: child.name,
+//           label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
+//         })),
+//       });
+//     }
 
-    return acc;
-  },
-  []
-);
+//     return acc;
+//   },
+//   []
+// );
 
 
 // * Programmactical way
