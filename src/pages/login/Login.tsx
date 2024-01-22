16470bd85b91f6bf8d5749c6fb7f5e5c37adb6e5
Md Mobassher Hossain <mobassherpautex@gmail.com>
2024-01-22 13:09:21 +0600

    set accesstoken to state

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,43 +1,42 @@
 import { Button } from 'antd';
-import {useForm  } from 'react-hook-form'
-import { useLoginMutation } from '../../redux/features/auth/authApi';
+import { useForm } from 'react-hook-form';
 import { useAppDispatch } from '../../redux/hooks';
+import { useLoginMutation } from '../../redux/features/auth/authApi';
+import { verifyToken } from '../../utils/verifyToken';
+import { setUser } from '../../redux/features/auth/authSlice';
 
 
 const Login = () => {
   const dispatch = useAppDispatch();
-   const {register, handleSubmit} = useForm({
-    defaultValues: {
-      userId: 'A-0001',
-      password: 'admin123',
-    },
-   })
-   const [login, { error}] = useLoginMutation()
+  const { register, handleSubmit } = useForm();
 
-   const onSubmit = async (data) => {
+  const [login, { error }] = useLoginMutation();
+
+  const onSubmit = async (data) => {
     const userInfo = {
       id: data.userId,
       password: data.password,
-    }
+    };
 
-    login(userInfo)
-   }
+    const res = await login(userInfo).unwrap();
+    const user = verifyToken(res.data.accessToken);
+
+    dispatch(setUser({ user: user, token: res.data.accessToken }));
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div>
-        <label htmlFor="id">User ID:</label>
-        <input type="text" id= "id" {...register('id')} />
+        <label htmlFor="id">ID: </label>
+        <input type="text" id="id" {...register('userId')} />
       </div>
       <div>
-        <label htmlFor="password">Password:</label>
-        <input type="text" id='password' {...register("password")} />
+        <label htmlFor="password">Password: </label>
+        <input type="text" id="password" {...register('password')} />
       </div>
-
-      <Button htmlType='submit'>Login</Button>
-
+      <Button htmlType="submit">Login</Button>
     </form>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
